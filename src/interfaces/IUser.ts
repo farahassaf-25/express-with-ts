3415cae13f6ea26c100f9b7692635849257047be
farahassaf-25 <assaffarah25@gmail.com>
2025-04-06 16:00:44 +0300

    create user interface

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,11 @@
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword: (password: string) => Promise<boolean>;
+}
