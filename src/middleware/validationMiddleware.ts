95fce8dc7e46c94fbb486d4f04f7bcc8edc059de
farahassaf-25 <assaffarah25@gmail.com>
2025-04-06 17:10:05 +0300

    create validation middleware

---
+++ src/middleware/validationMiddleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import { AnyZodObject, ZodError } from 'zod';
+import ApiError from '../utils/ApiError';
+
+const validate = (schema: AnyZodObject) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errors = error.errors.map((err) => ({
+          path: err.path.join('.'),
+          message: err.message,
+        }));
+        next(new ApiError(400, 'Validation Error', false, errors));
+      } else {
+        next(error);
+      }
+    }
+  };
+};
+
+export default validate;
