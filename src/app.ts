f65e3327e43a4fa37322cac2855974f852bd94d6
farahassaf-25 <assaffarah25@gmail.com>
2025-04-06 15:11:46 +0300

    add NODE_ENV

--- src/app.ts
+++ src/app.ts
@@ -57,6 +57,7 @@ class App {
             res.status(500).json({
                 message: 'Internal Server Error',
                 status: false,
+                error: process.env.NODE_ENV === 'development' ? err.message : undefined,
             });
         });
     }
